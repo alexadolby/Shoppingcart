817a3a0e1365e6cfb80874b3c8cb0c91e0975c33
Alexa Dolby <59667085+alexadolby@users.noreply.github.com>
2020-02-07 20:54:30 -0500

    lookup product given identifier

--- shopping_cart.py
+++ shopping_cart.py
@@ -29,9 +29,18 @@ print(products)
 
 
 selected_id = input("Please input a product identifier: ")
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+#print(matching_product)
+#print(type(matching_product))
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
-print(selected_id)
-print(type(selected_id))
+#print(matching_product)
+#print(type(matching_product))
+
+
+#print(selected_id)
+#print(type(selected_id))
 
 
 
